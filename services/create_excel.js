9195d3b392c9a6dc4ba18f6c1c40075ebc7c1fa4
Serm135 <pinilloss@uninorte.edu.co>
2023-02-28 11:32:04 -0500

    Excel 2

--- services/create_excel.js
+++ services/create_excel.js
@@ -12,9 +12,43 @@ router.post('/', async(req,res)=>{
     workbook.xlsx.readFile(excel)
     .then(function() {
         const worksheet = workbook.getWorksheet(1);
-        var row = worksheet.getRow(6);
-        row.getCell(2).value = 'Alberto Mario'; 
-        row.commit();
+        worksheet.getCell('B6').value = 'Alberto'; //Cliente
+        worksheet.getCell('F6').value = '12/12/1999'; //Fecha visita
+        worksheet.getCell('B8').value = 'Malecon'; //Nombre del sitio
+        worksheet.getCell('F8').value = 'Calle ...'; //Dirección
+        worksheet.getCell('B10').value = 'La obra va joya'; //Descripción breve de la obra
+        worksheet.getCell('F10').value = 'Calle ... Carrera'; //Dirección 2
+        worksheet.getCell('A15').value = 'Falta cantidad importante de materiales asdkjfa sdlfjals kdjfal;skdjf;lajsd lfajslfdjasldfjaks hdfkahsdfkjahskd hfkasdf asdfasdfasldfjalsdjfljalsj laskdjflaksjdlfkajsldkfj alskdjflasjdlfkjls djflas jlfkj sld jlaskdjfl ajsldfkajsl'; //Comentarios Relevantes de la visitade obra
+        //IF para la cantidad de tipo de trabajos que sean necesarios + añadir más columnas si es necesario
+        worksheet.getCell('A19').value = 'Transporte de materiales'; //Tipo de trabajos
+        worksheet.getCell('B19').value = 'Llevar el cemento'; //Descripción del alcance
+        //Fotos desarrollo del trabajo
+        const imageId1 = workbook.addImage({
+          filename: 'files/foto1.jpeg',
+          extension: 'jpeg',
+        });
+        worksheet.addImage(imageId1, {
+          tl: { col: 0, row: 27.2 },
+          ext: { width: 260, height: 230 }
+        });
+        worksheet.addImage(imageId1, {
+          tl: { col: 2.5, row: 27.2 },
+          ext: { width: 350, height: 230 }
+        });
+        worksheet.addImage(imageId1, {
+          tl: { col: 3.5, row: 27.2 },
+          ext: { width: 350, height: 230 }
+        });
+
+        worksheet.getCell('C38').value = 'Juan Carlos'; //Nombre de quien visitó y elabora el informe
+        const imageId2 = workbook.addImage({
+          filename: 'files/firma.jpg',
+          extension: 'jpg',
+        });
+        worksheet.addImage(imageId2, {
+          tl: { col: 4.7, row: 37.3 },
+          ext: { width: 230, height: 73 }
+        });
         res.status(202).json('Ok')
         return workbook.xlsx.writeFile('files/new.xlsx');
     });
